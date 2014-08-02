938b6197daa233bd15aef70034e89c6e3b013190
suzuki1964 <suzuki1964@mac.com>
2014-08-02 14:24:45 -0500

    Episode 3 -- set up mod class

---
+++ src/main/java/com/ksuzuki/hunter/Hunter.java
@@ -0,0 +1,36 @@
+package com.ksuzuki.hunter;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by suzuki on 8/2/14.
+ */
+
+@Mod(modid="Hunter", name="Emi's Hunter Mod", version="1.7.10-1.0")
+
+public class Hunter
+{
+    @Mod.Instance("Hunter")
+    public static Hunter instance;
+
+    @Mod.EventHandler
+    public void PreInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void PostInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
